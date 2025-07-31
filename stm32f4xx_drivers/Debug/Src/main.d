20a08f3706db540aabf1c9a1901c7f9e61f64682
Jaspreet Singh <115124290+jaz404@users.noreply.github.com>
2025-07-30 22:52:06 -0400

    added base addr file

---
+++ stm32f4xx_drivers/Debug/Src/main.d
@@ -0,0 +1,3 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/jaspr/STM32CubeIDE/workspace_1.16.1/MCU1/mcu1_section00/stm32f4xx_drivers/Drivers/Inc/stmf446xx.h
+C:/Users/jaspr/STM32CubeIDE/workspace_1.16.1/MCU1/mcu1_section00/stm32f4xx_drivers/Drivers/Inc/stmf446xx.h:
---
+++ stm32f4xx_drivers/Drivers/Inc/stmf446xx.h
@@ -0,0 +1,69 @@
+/*
+ * stmf446xx.h
+ *
+ *  Created on: Jul 27, 2025
+ *      Author: jaspr
+ */
+
+#ifndef INC_STMF446XX_H_
+#define INC_STMF446XX_H_
+
+/*
+ * base addresses of memories - SRAM, FLASH, ROM
+ * all these numbers are in memory map, ref manual (page 65)
+ */
+
+#define FLASH_BASEADDR				0x08000000U
+#define SRAM1_BASEADDR				0x20000000U // 112KB
+#define SRAM2_BASEADDR				0x2001C000U // 16KB, 0x20000000U + hex of 112 * 1024 by
+#define ROM_BASEADDR				0x1FFF0000U
+#define SRAM 						SRAM1_BASEADDR
+
+/*
+ * AHBx and APBx Bus Peripheral base addresses
+ */
+
+#define PERIPH_BASE					0x40000000U
+#define APB1PERIPH_BASEADDR			PERIPH_BASE
+#define APB2PERIPH_BASEADDR			0x40010000U
+#define AHB1PERIPH_BASEADDR			0x40020000U
+#define AHB2PERIPH_BASEADDR			0x40030000U
+
+/*
+ * base addresses of peripherals hanging on the AHB1 bus
+ */
+#define GPIOA_BASEADDR			(AHB1PERIPH_BASEADDR + 0x0000)
+#define GPIOB_BASEADDR			(AHB1PERIPH_BASEADDR + 0x0400)
+#define GPIOC_BASEADDR			(AHB1PERIPH_BASEADDR + 0x0800)
+#define GPIOD_BASEADDR			(AHB1PERIPH_BASEADDR + 0x0C00)
+#define GPIOE_BASEADDR			(AHB1PERIPH_BASEADDR + 0x1000)
+#define GPIOF_BASEADDR			(AHB1PERIPH_BASEADDR + 0x1400)
+#define GPIOG_BASEADDR			(AHB1PERIPH_BASEADDR + 0x1800)
+#define GPIOH_BASEADDR			(AHB1PERIPH_BASEADDR + 0x1C00)
+#define GPIOI_BASEADDR			(AHB1PERIPH_BASEADDR + 0x2000)
+
+/*
+ * base addresses of peripherals hanging on the APB1 bus
+ */
+#define I2C1_BASEADDR			(APB1PERIPH_BASEADDR + 0x5400)
+#define I2C2_BASEADDR			(APB1PERIPH_BASEADDR + 0x5800)
+#define I2C3_BASEADDR			(APB1PERIPH_BASEADDR + 0x5C00)
+#define SPI2_BASEADDR			(APB1PERIPH_BASEADDR + 0x3800)
+#define SPI3_BASEADDR			(APB1PERIPH_BASEADDR + 0x3C00)
+#define USART2_BASEADDR			(APB1PERIPH_BASEADDR + 0x4400)
+#define USART3_BASEADDR			(APB1PERIPH_BASEADDR + 0x4800)
+#define UART4_BASEADDR			(APB1PERIPH_BASEADDR + 0x4C00) // not USART! not synchronous
+#define UART5_BASEADDR			(APB1PERIPH_BASEADDR + 0x5000) // not USART! not synchronous
+
+/*
+ * base addresses of peripherals hanging on the APB2 bus
+ */
+#define EXTI_BASEADDR 			(APB2PERIPH_BASEADDR + 0x3C00)
+#define SYSCFG_BASEADDR 		(APB2PERIPH_BASEADDR + 0x3800)
+#define USART1_BASEADDR			(APB2PERIPH_BASEADDR + 0x1000)
+#define USART6_BASEADDR			(APB2PERIPH_BASEADDR + 0x1400)
+#define SPI1_BASEADDR			(APB2PERIPH_BASEADDR + 0x3000)
+#define SPI4_BASEADDR			(APB2PERIPH_BASEADDR + 0x3400)
+
+
+#endif /* INC_STMF446XX_H_ */
